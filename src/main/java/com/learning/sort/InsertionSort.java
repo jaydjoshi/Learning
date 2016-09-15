24ed2ae127d78af92ccd1b51bc1e59e663bc2e53
Jay Joshi <joshi.jay.d@gmail.com>
2016-09-15 20:45:21 +0530

    insertion sort

--- src/main/java/com/learning/sort/InsertionSort.java
+++ src/main/java/com/learning/sort/InsertionSort.java
@@ -15,9 +15,11 @@ public class InsertionSort {
 		for (int i = 1; i < length; i++) {
 			value=arr[i];
 			hole=i;
-			while(hole>0 && arr[hole]>value){
-				
+			while(hole>0 && arr[hole-1]>value){
+				arr[hole]=arr[hole-1];
+				hole--;
 			}
+			arr[hole]=value;
 		}
 		return arr;
 	}
