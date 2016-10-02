621c2ad839800a7b7d65f744c52f046bf384a048
Jay Joshi <joshi.jay.d@gmail.com>
2016-10-02 20:34:26 +0530

    Serialization example

--- src/main/java/com/learning/serialization/SerializationMain.java
+++ src/main/java/com/learning/serialization/SerializationMain.java
@@ -15,20 +15,22 @@ public class SerializationMain {
 		String fileName = "C:/Jay/Personnel work/Learning/learning/src/main/java/com/learning/serialization/employeeSerialized.txt";
 		String fileName1 = "C:/Jay/Personnel work/Learning/learning/src/main/java/com/learning/serialization/employeeSerialized.ser";
 		
-		SerializationUtil.writeObjectToFile(emp,fileName);
+		SerializationUtil.writeObjectToFile(emp,fileName1);
 		
 		System.out.println("Object serialized "+emp.getName());
+		System.out.println(emp.getSalary());
 		System.out.println(emp.hashCode());
 		
 		Employee emp2 = null;
 		try {
-			emp2 = SerializationUtil.readObjectFromFile(fileName);
+			emp2 = SerializationUtil.readObjectFromFile(fileName1);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		System.out.println("Object deserialized "+emp2.getName());
+		System.out.println(emp2.getSalary());
 		System.out.println(emp2.hashCode());
 
 	}
