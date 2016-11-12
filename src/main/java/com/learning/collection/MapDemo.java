c867db2105e1ebf79791669a29a75590dba4d1ed
Jay Joshi <joshi.jay.d@gmail.com>
2016-11-12 23:24:12 +0530

    Map demo

--- src/main/java/com/learning/collection/MapDemo.java
+++ src/main/java/com/learning/collection/MapDemo.java
@@ -6,20 +6,26 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MapDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Map<String,Integer> map = new HashMap<>();
+		//Map<String,Integer> map = new HashMap<>();
 		//Map<String,Integer> map = new LinkedHashMap<>();
-		//Map<String,Integer> map = new TreeMap<>();
+		Map<String,Integer> map = new TreeMap<>();
+		Map<String,Integer> conMap = new ConcurrentHashMap<String, Integer>();
 		map.put("jay", 1);
 		map.put("yaj", 2);
 		map.put("ajy", 3);
-		map.put(null, 2);
-		
+		map.put("abc", 4);
+		map.put("kat", 6);
+		map.put("dog", 7);
+		map.put("zebra", 5);
+		map.put("zebri", 8);
+		map.put("jayu", 10);
 		String s = new String("a");
 		
 		/*for (Entry entry : map.entrySet()) {
